740e780b0c3792e1e444f1d6641de8ef40e857d7
secondlife <secondlife@90db1341-1322-0410-8ffa-a12b5144e862>
2007-12-26 08:38:22 +0000

    git-svn-id: svn+ssh://rubyforge.org/var/svn/hotchpotch/rascut/trunk@50 90db1341-1322-0410-8ffa-a12b5144e862

--- lib/rascut/plugin/screen.rb
+++ lib/rascut/plugin/screen.rb
@@ -4,11 +4,12 @@ require 'rascut/plugin/base'
 module Rascut
   module Plugin
     class Screen < Base
-      STATUS = %q[%H %`%-w%{=b bw}%n %t%{-}%+w]
+      STATUS = %q[%H %`%-w%{=b bw}%n %t%{-}%+w %=]
 
       SCREEN_CMD = 'screen'
       SCREEN_COLOR = {
         :black => 'dd',
+        :blue  => 'bk',
         :green => 'gk',
         :red   => 'rw',
       }
@@ -20,7 +21,7 @@ module Rascut
       end
 
       def start
-        message 'S'
+        message 'C'
       end
 
       def error
@@ -28,7 +29,7 @@ module Rascut
       end
 
       def success
-        message 'S', :green
+        message 'S', :blue
       end
 
       def message(msg, color = :black)
