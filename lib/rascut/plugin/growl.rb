c497611f618405bc2243d6cd14df5cb72aebc5ad
Makoto Tokuyama <marugoshi@me.com>
2011-09-02 12:15:59 +0900

    add growl plugin.

---
+++ lib/rascut/plugin/growl.rb
@@ -0,0 +1,28 @@
+require "rascut/plugin/base"
+
+module Rascut
+  module Plugin
+    class Growl < Base
+      def run
+        @growl = `which growlnotify`.chomp
+        return unless @growl
+        @command.wrapper.hooks[:compile_error] << method(:error)
+        @command.wrapper.hooks[:compile_success] << method(:success)
+      end
+
+      def success(str)
+        command = %Q[#{@growl} -w -m 'success' 'rascut compile results']
+        system command
+      end
+
+      def error(str)
+        command = %Q[#{@growl} -w -m 'failed' 'rascut compile results']
+        system command
+      end
+
+      def strip_ansi(m)
+        m.gsub(/\e\[[^m]*m/, '')
+      end
+    end
+  end
+end
